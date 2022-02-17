d5524f3a8f8d217c8d2240964bc807be731c31bd
sjinmin5 <98172702+sjinmin5@users.noreply.github.com>
2022-02-17 14:08:07 -0500

    Update Card.java
    added my last name

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.project;
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
  * @author dancye
- * @author Suhjin
+ * @author Suhjin_Min
  */
 public abstract class Card {
     //default modifier for child classes
