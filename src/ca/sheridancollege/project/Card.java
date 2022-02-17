a9be8efc1e6c47c351cc38ad3830987a276508ba
IvyQiong <liaoq@sheridancollege.ca>
2022-02-17 13:58:56 -0500

    add name again

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,7 +2,7 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
- *Date: Feb 17 2022 Qiong
+ *Date: Feb 17 2022 IvyQiong
  */
 package ca.sheridancollege.project;
 
