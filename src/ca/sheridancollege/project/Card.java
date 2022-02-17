058ae8a1016dfaf22b44d145c7caa4dd85133a18
IvyQiong <98172790+IvyQiong@users.noreply.github.com>
2022-02-17 13:43:44 -0500

    Update Card.java
    Add date and name

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,6 +2,7 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ *Date 2022-02-17 -Qiong
  */
 package ca.sheridancollege.project;
 
